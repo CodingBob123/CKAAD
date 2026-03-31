3f0cc37cbff9173e3cfd5709a9fe514c88e701bf
bobby <3067748196@qq.com>
2026-03-31 05:57:57 +0000

    fix:修复checkpoint-dir路径错误问题

--- mvtec_day1.sh
+++ mvtec_day1.sh
@@ -7,9 +7,10 @@ labeled_anomaly_ratio=0.05
 labeled_anomaly_class_num=1
 checkpoint_interval=80          # 每 多少 个 epoch 保存一次 checkpoint
 checkpoint_mode=best         # 加载最新保存的 checkpoint
-# checkpoint_dir=./hy-tmp/checkpoints  # checkpoint 存储根目录；设为空或不设置则默认保存在项目根目录的 ./checkpoints 下
+checkpoint_dir=/hy-tmp/checkpoints  # checkpoint 存储根目录；设为空或不设置则默认保存在项目根目录的 ./checkpoints 下
 
-for normal in 'bottle' 'cable' 'capsule' 'carpet' 'grid' 'hazelnut' 'leather' 'metal_nut' 'pill' 'screw' 'tile' 'toothbrush' 'transistor' 'wood' 'zipper'
+# for normal in 'bottle' 'cable' 'capsule' 'carpet' 'grid' 'hazelnut' 'leather' 'metal_nut' 'pill' 'screw' 'tile' 'toothbrush' 'transistor' 'wood' 'zipper'
+for normal in 'grid' 'hazelnut' 'leather' 'metal_nut' 'pill' 'screw' 'tile' 'toothbrush' 'transistor' 'wood' 'zipper'
 # for normal in 'capsule' 'carpet' 'grid' 'metal_nut' 'pill' 'screw' 'tile' 'toothbrush' 'transistor' 'wood' 'zipper'
 do
     echo "[Day1 Baseline] Training: $normal"
@@ -47,7 +48,7 @@ do
     # 基础命令：关闭能量图融合，使用 --recon_only 跳过 D(input) 能量图融合
     base_args=(
         --dataset mvtec
-        --batch_size 8
+        --batch_size 16
         --lr ${lr}
         --d_lr 1e-04
         --adv_conf 0.02
@@ -66,8 +67,9 @@ do
         # --recon_only                    # 核心：跳过能量图融合，使用纯重建误差图
         --checkpoint_interval ${checkpoint_interval}
         --checkpoint_mode ${checkpoint_mode}
+        --checkpoint_dir ${checkpoint_dir}
         --use_amp
-        --energy_diff_mode
+        # --energy_diff_mode
     )
 
     echo ">>> CMD: python main.py ${base_args[*]}"
